23405f552120c74f024252a3a791c4471316d5ad
Noviyanto <noviyanto26@gmail.com>
2026-02-03 12:55:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def check_password() -> bool:
 
     with col2:
         with st.container(border=True):
-            st.markdown("<h2 style='text-align: center;'>🔐 Login Dashboard</h2>", unsafe_allow_html=True)
+            st.markdown("<h2 style='text-align: center;'>🔐 Login Dashboard Registry Hemofilia</h2>", unsafe_allow_html=True)
             st.info("Silakan masukkan username dan password Anda.")
 
             # --- PERUBAHAN UTAMA: Membungkus input dalam st.form ---
