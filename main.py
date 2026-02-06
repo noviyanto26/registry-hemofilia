944617b9e2be283039ad338a00a0b2eb906fee2b
Noviyanto <noviyanto26@gmail.com>
2026-02-06 13:25:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def check_password() -> bool:
 # Menambahkan 01a_tampil_data.py ke daftar menu
 FULL_MENU_ITEMS = {
     "📝 Input Data Hemofilia": "01_pwh_input.py",
-    "📋 Data Lengkap Pasien": "01a_tampil_data.py",  # <--- ITEM BARU
+    "📋 Tampil Data": "01a_tampil_data.py",  # <--- ITEM BARU
     "📊 Rekapitulasi per Kelompok Usia": "02_rekap_pwh.py",
     "🚻 Rekapitulasi per Jenis Kelamin": "03_rekap_gender.py",
     "🏥 RS Perawatan Hemofilia": "04_rs_hemofilia.py",
